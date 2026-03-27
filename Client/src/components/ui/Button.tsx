d5131a74f283960baf34143274ebb3c85a28a67a
Rahul Yadav <ry9064526713@gmail.com>
2026-03-27 23:38:46 +0530

    made button component and used them

--- Client/src/components/ui/Button.tsx
+++ Client/src/components/ui/Button.tsx
@@ -5,14 +5,28 @@ export interface ButtonProps {
     text : string;
     startIcon ? : number;
     endIcon ? : number;
-    onClick : () => void;
+    onClick? : () => void;
 }
+
+const variantOptions = {
+    "primary" : "bg-[#4543DA] text-white",
+    "secondary" : "bg-[#d6d6ff] text-[e0e7fe]"
+}
+
+const sizeStyles = {
+    "sm" : "py-1 px-2", // p in tailwind css is for padding, x and y are axis
+    "md" : "py-2 px-4",
+    "lg" : "py-4 px-6"
+}
+
+const defaultStyles = "rounded-md" //rounded is for the corners(like border-radius)
 
 export const Button = (props : ButtonProps) => {
     return (
-        <button onclick = {onClick}>
-            {props.variant}
-            
+        <button className={`${variantOptions[props.variant]} 
+            ${defaultStyles} ${sizeStyles[props.size]}`}
+        >  
+        {props.text}
         </button>
  );
 }
